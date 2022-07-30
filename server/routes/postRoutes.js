f5eb78b21b321ac8b6841285d4064178e75304ce
fasilkp <fasilkp314@gmail.com>
2022-07-30 21:20:14 +0530

    verifyLogin middleware added

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router=express.Router()
 import {postDetails, addPost} from '../controllers/postController.js'
-
+import verifyLogin from '../middlewares/verifyLogin.js'
 
 router.get("/", postDetails);
-router.post("/add-post", addPost);
+router.post("/add-post",verifyLogin, addPost);
 
 
 export default router;
