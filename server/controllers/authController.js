a1e2474575a6a0aded9a35373699ea93aa8083ce
fasilkp <fasilkp314@gmail.com>
2022-07-30 20:42:06 +0530

    regsiter user route completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,28 +1,41 @@
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcryptjs"
+
+var salt = bcrypt.genSaltSync(10);
 
 export const registerUser = (req, res) => {
   try {
-    const user = new UserModel(req.body);
-    user.save((err)=>{
-        if(err){
-            return res.json({ register: false, message: err });
-        }
-        else{
-            return res.json({ register: true, message: "user logged in" });
-        }
-    })
+    const {userName, name, email, password} = req.body;
+    const hashPassword = bcrypt.hashSync(password, salt);
+    const user = new UserModel({
+      userName,
+      name,
+      email,
+      password:hashPassword,
+    });
+    user.save((err) => {
+      if (err) {
+        return res.json({ register: false, message: err });
+      } else {
+        return res.json({ register: true, message: "registration successfull" });
+      }
+    });
   } catch (err) {
     res.json({ register: false, message: err });
   }
 };
 export const loginUser = async (req, res) => {
-
-    const user = await UserModel.findOne({ email: req.body.email, password: req.body.password });
-    if(user){
-        return res.json({login:true, message:"logged in successfully", user})
-    }
-    else{
-        return res.json({login:false, message:"logged in failed", err:"invalid email or password"})
-    }
-
+  const {email, password}=req.body
+  const user = await UserModel.findOne({
+    email
+  });
+  if(!user) {
+    return res.json({
+      login: false,
+      message: "logged in failed",
+      err: "invalid email or password",
+    });
+  }
+  
+  return res.json({ login: true, message: "logged in successfully", user });
 };
