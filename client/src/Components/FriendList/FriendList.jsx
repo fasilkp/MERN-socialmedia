f448c8d1080b140f898c0cb5402c945d11a70f28
fasilkp <fasilkp314@gmail.com>
2022-08-10 21:38:06 +0530

    friends list page makes responsive

--- client/src/Components/FriendList/FriendList.jsx
+++ client/src/Components/FriendList/FriendList.jsx
@@ -88,6 +88,159 @@ function FriendList() {
           <button>follow</button>
         </div>
       </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
+      <div className="single-frnd">
+        <div className="fr-img-details">
+          <div className="fr-img">
+            <img
+              src="http://localhost:8080/images/profile-images/defaultImage.jpg"
+              alt=""
+            />
+          </div>
+          <div className="frnd-details">
+            <span>fasilkp</span>
+            <span>Mohamed Fasil Kp</span>
+          </div>
+        </div>
+        <div className="frnd-btn">
+          <button>follow</button>
+        </div>
+      </div>
     </div>
   );
 }
