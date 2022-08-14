9b907361158c78020a6afa229e997c52f9942c21
fasilkp <fasilkp314@gmail.com>
2022-08-14 22:37:07 +0530

    username validator hook added to register page

--- client/src/Components/registerComponent/RegisterComp.jsx
+++ client/src/Components/registerComponent/RegisterComp.jsx
@@ -1,28 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './RegisterComp.css'
 import loginImg from '../../images/login.jpg'
 import { Link, useNavigate } from "react-router-dom";
+import { useNameValidator } from "../../hooks/useNameValidator";
 import BeatLoader from "react-spinners/BeatLoader";
 import Axios from 'axios'
 function RegisterComp() {
-  const [submitLoad,setSubmitLoad]=useState(false)
-  const [name, setName]=useState("");
-  const [userName, setUserName]=useState("");
-  const [password, setPassword]=useState("");
-  const [email, setEmail]=useState("");
-  const navigate=useNavigate()
-  const handleChange=(e, setState)=>{
-    eval(setState+"(e.target.value)")
+  const [submitLoad, setSubmitLoad] = useState(false)
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userNameValidMessage, setUserNameValidMessage] = useState(null);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate()
+  const handleChange = (e, setState) => {
+    eval(setState + "(e.target.value)")
   }
-  const handleSubmit=async e=>{
-    setSubmitLoad(true)
+  const ValidateUserName = (e) => {
+    const validatorMessage=useNameValidator(e.target.value);
+    setUserNameValidMessage(validatorMessage)
+  }
+  const handleSubmit = async e => {
     e.preventDefault();
-    const user = await Axios.post("/auth/register", {name, email, password, userName:userName.toLowerCase()});
-    if(user.data.register){
+    setSubmitLoad(true)
+    const user = await Axios.post("/auth/register", { name, email, password, userName: userName.toLowerCase() });
+    if (user.data.register) {
       alert("register successfull");
       navigate('/login')
     }
-    else{  
+    else {
       alert(user.data.message);
     }
     setSubmitLoad(false)
@@ -38,19 +44,22 @@ function RegisterComp() {
           <span>Register Your Account</span>
         </div>
         <div className="reg-body">
-            <input type="text" placeholder="Name" className="reg-input" 
-             value={name} onChange={(e)=>handleChange(e,'setName')} />
-            <input type="text" placeholder="Username (note: should be unique)" className="reg-input" 
-             value={userName.toLowerCase()} onChange={(e)=>handleChange(e,'setUserName')} />
-            <input type="email" placeholder="Email" className="reg-input" 
-             value={email} onChange={(e)=>handleChange(e,'setEmail')} />
-            <input type="password" placeholder="Password" className="reg-input" 
-             value={password} onChange={(e)=>handleChange(e,'setPassword')} />
-            <button className="reg-btn" onClick={handleSubmit}>
-              {
-              submitLoad ? <BeatLoader size="15" color="white"/> : "Login"
-              }
-            </button>
+          <input type="text" placeholder="Name" className="reg-input"
+            value={name} onChange={(e) => handleChange(e, 'setName')} />
+          <input type="text" placeholder="Username (note: should be unique)" className="reg-input"
+            value={userName.toLowerCase()} onChange={(e) => {handleChange(e, 'setUserName'); ValidateUserName(e)}} />
+          <span className="validator-message">
+            {userNameValidMessage?.message}
+          </span>
+          <input type="email" placeholder="Email" className="reg-input"
+            value={email} onChange={(e) => handleChange(e, 'setEmail')} />
+          <input type="password" placeholder="Password" className="reg-input"
+            value={password} onChange={(e) => {handleChange(e, 'setPassword'); }} />
+          <button className="reg-btn" onClick={handleSubmit}>
+            {
+              submitLoad ? <BeatLoader size="15" color="white" /> : "Login"
+            }
+          </button>
         </div>
         <Link className="links" to="/login"><div className="another-link">Already Have an Acoount? Please Login!</div></Link>
       </div>
