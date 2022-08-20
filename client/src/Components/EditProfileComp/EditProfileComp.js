bd961af75b0d8f8232a46bfa83e414fbbaae2bb9
fasilkp <fasilkp314@gmail.com>
2022-08-20 21:08:45 +0530

    edit profile ui completed

--- client/src/Components/EditProfileComp/EditProfileComp.js
+++ client/src/Components/EditProfileComp/EditProfileComp.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import './EditProfileComp.css'
 function EditProfileComp() {
   return (
-    <div>EditProfileComp</div>
+    <div className='EditProfileComp'>
+      <div className="edit-pf-container">
+        <div className="pf-img-col">
+          <img src="http://localhost:8080/images/profile-images/defaultImage.jpg" alt=""/>
+          <label htmlFor="imageInput">
+            change profile picture
+          </label>
+            <input type="file" id="imageInput" />
+        </div>
+        <div className="pf-form-col">
+          <label htmlFor="">Name</label>
+          <input type="text" placeholder="name..." />
+          <label htmlFor="">Email</label>
+          <input type="text" placeholder="email..." />
+          <button>
+          Save
+          </button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
